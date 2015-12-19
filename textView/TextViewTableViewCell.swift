f4c3a8f534209487ab76795267fcf0b757a69336
Brad G <bgay@iMac-Living-Room.local>
2015-12-18 23:35:50 -0500

    first commit

---
+++ textView/TextViewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TextViewTableViewCell.swift
+//  textView
+//
+//  Created by Brad G. on 12/18/15.
+//  Copyright © 2015 Brad G. All rights reserved.
+//
+
+import UIKit
+
+class TextViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
